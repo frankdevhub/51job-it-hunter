fd0a680c3bdb0e3a09730da9b5fecc24553242ac
frankdevhub <frankdevhub@gmail.com>
2019-12-05 17:43:54 +0800

    手机号码格式校验

--- Examples/Examples/UnitTest/phoneNumRegx.cpp
+++ Examples/Examples/UnitTest/phoneNumRegx.cpp
@@ -1,8 +1,34 @@
 #include "../Common/cn_mobile.h"
 #include <iostream>
+#include <string.h>
+#include <stdio.h>
+
+void printRes(char* res)
+{
+	if (NULL == res)
+		printf("ERROR FORMAT:[%s]", res);
+	else
+		printf("CORRENT:[%s]", res);
+
+	printf("\n");
+	res = NULL;
+}
 
 int main()
 {
-	std::cout << "start" << std::endl;
+	printf("start");
+	char* res;
+	char *phone = (char*)"13585844052";
+	res = ::GetCNLocalPhoneNum(phone);
+	printRes(res);
+
+	phone = (char*)"23583374052";
+	res = ::GetCNLocalPhoneNum(phone);
+	printRes(res);
+
+	phone = (char*)"10444433978";
+	res = ::GetCNLocalPhoneNum(phone);
+	printRes(res);
+
 	system("pause");
 }
