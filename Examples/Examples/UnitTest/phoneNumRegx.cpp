af4b929b56863c1681a0c6af5a38d69944c8c17a
frankdevhub <frankdevhub@gmail.com>
2019-12-05 21:15:41 +0800

    手机号码格式校验测试

--- Examples/Examples/UnitTest/phoneNumRegx.cpp
+++ Examples/Examples/UnitTest/phoneNumRegx.cpp
@@ -14,21 +14,21 @@ void printRes(char* res)
 	res = NULL;
 }
 
-int main()
-{
-	printf("start");
-	char* res;
-	char *phone = (char*)"13585844052";
-	res = ::GetCNLocalPhoneNum(phone);
-	printRes(res);
-
-	phone = (char*)"23583374052";
-	res = ::GetCNLocalPhoneNum(phone);
-	printRes(res);
-
-	phone = (char*)"10444433978";
-	res = ::GetCNLocalPhoneNum(phone);
-	printRes(res);
-
-	system("pause");
-}
+//int main()
+//{
+//	printf("start");
+//	char* res;
+//	char *phone = (char*)"13585844052";
+//	res = ::GetCNLocalPhoneNum(phone);
+//	printRes(res);
+//
+//	phone = (char*)"23583374052";
+//	res = ::GetCNLocalPhoneNum(phone);
+//	printRes(res);
+//
+//	phone = (char*)"10444433978";
+//	res = ::GetCNLocalPhoneNum(phone);
+//	printRes(res);
+//
+//	system("pause");
+//}
--- frankdevhub-51job-client/frankdevhub-51job-client/phoneRegex.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <stdio.h>
-#include "cn_mobile.h"
-
-int main()
-{
-	printf("test info\n");
-	system("pause");
-}
