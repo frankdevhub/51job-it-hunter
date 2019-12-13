1d5c3251dd14e689a80d819d4279e0fb7db1addd
frankdevhub <frankdevhub@gmail.com>
2019-12-13 21:59:04 +0800

    解析Get和Post请求返回的Json数据

--- Examples/Examples/Common/cn_mobile.cpp
+++ Examples/Examples/Common/cn_mobile.cpp
@@ -15,14 +15,20 @@ bool GetCNLocalPhoneNum(char* phoneNum)
 
 void GetLocalInfo(char *phoneNum)
 {
-	CWininetHttp http;
+	CWininetHttp http = CWininetHttp();
 	Json::Value jsonValue;
 	std::string strlpUrl = "http://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=";
 	strlpUrl.append(phoneNum);
 
-	//spdlog::info("using phoneNum;{}", phoneNum);
-	//spdlog::info("strHostName:{}", strlpUrl.c_str());
-	jsonValue = http.RequestJsonInfo(strlpUrl, Hr_Get, NULL, NULL);
+	spdlog::info("using phoneNum;{}", phoneNum);
+	const std::string lpUrl = (const string)strlpUrl.c_str();
+	spdlog::info("strHostName:{}", lpUrl);
+	if (NULL == &http)
+		spdlog::critical("variable {},is NULL", "http");
+	if (NULL == &jsonValue)
+		spdlog::critical("variable {},is NULL", "jsonValue");
+
+	http.RequestJsonInfo(lpUrl, Hr_Get, NULL, NULL);
 	//spdlog::info("response value:{}", jsonValue);
 
 }
