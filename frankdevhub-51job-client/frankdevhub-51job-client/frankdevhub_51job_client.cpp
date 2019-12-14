d458d20d53efc44d9e382a7cc1767e302d2bb5ac
frankdevhub <frankdevhub@gmail.com>
2019-12-14 23:32:25 +0800

    客户端UI界面初始化

--- frankdevhub-51job-client/frankdevhub-51job-client/frankdevhub_51job_client.cpp
+++ frankdevhub-51job-client/frankdevhub-51job-client/frankdevhub_51job_client.cpp
@@ -1,9 +1,9 @@
-//#include "frankdevhub_51job_client.h"
-//#include "ui_frankdevhub_51job_client.h"
-//
-//frankdevhub_51job_client::frankdevhub_51job_client(QWidget *parent)
-//	: QMainWindow(parent)
-//{
-//	ui.setupUi(this);
-//	
-//}
+#include "frankdevhub_51job_client.h"
+#include "ui_frankdevhub_51job_client.h"
+
+frankdevhub_51job_client::frankdevhub_51job_client(QWidget *parent)
+	: QMainWindow(parent)
+{
+	ui.setupUi(this);
+	
+}
