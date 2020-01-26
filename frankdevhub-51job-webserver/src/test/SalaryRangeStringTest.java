500974fb3d9e7220ab4a344c09be4c2dd35764b7
FangChenSheng <frankdevhub@gmail.com>
2020-01-26 23:51:30 +0800

    add junit function test

---
+++ frankdevhub-51job-webserver/src/test/SalaryRangeStringTest.java
@@ -0,0 +1,28 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * <p>Title:@ClassName SalaryRangeStringTest.java</p>
+ * <p>Copyright: Copyright (c) 2020</p>
+ * <p>Company: www.frankdevhub.site</p>
+ * <p>github: https://github.com/frankdevhub</p>
+ *
+ * @Author: frankdevhub@gmail.com
+ * @CreateDate: 2020/1/26 23:49
+ * @Version: 1.0
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SalaryRangeStringTest {
+    @Before
+    public void init() {
+
+    }
+
+    @After
+    public void after() {
+
+    }
+}
