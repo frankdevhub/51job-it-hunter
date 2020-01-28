d23416b6d2896168206f15cfc9413babf208fb8a
FangChenSheng <frankdevhub@gmail.com>
2020-01-28 23:36:49 +0800

    common business utils

---
+++ frankdevhub-51job-webserver/src/main/java/frankdevhub/job/automatic/core/utils/CommonBusinessUtils.java
@@ -0,0 +1,16 @@
+package frankdevhub.job.automatic.core.utils;
+
+/**
+ * <p>Title:@ClassName CommonBusinessUtils.java</p>
+ * <p>Copyright: Copyright (c) 2020</p>
+ * <p>Company: www.frankdevhub.site</p>
+ * <p>github: https://github.com/frankdevhub</p>
+ *
+ * @Author: frankdevhub@gmail.com
+ * @CreateDate: 2020/1/28 23:36
+ * @Version: 1.0
+ */
+public class CommonBusinessUtils {
+    
+
+}
