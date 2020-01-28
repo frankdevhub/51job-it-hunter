1a1d5a0d69ed47fb6fddf55e925f0241be86cbaa
FangChenSheng <frankdevhub@gmail.com>
2020-01-28 22:43:54 +0800

    business constants

---
+++ frankdevhub-51job-webserver/src/main/java/frankdevhub/job/automatic/core/constants/BusinessConstants.java
@@ -0,0 +1,14 @@
+package frankdevhub.job.automatic.core.constants;
+
+/**
+ * <p>Title:@ClassName BusinessConstants.java</p>
+ * <p>Copyright: Copyright (c) 2020</p>
+ * <p>Company: www.frankdevhub.site</p>
+ * <p>github: https://github.com/frankdevhub</p>
+ *
+ * @Author: frankdevhub@gmail.com
+ * @CreateDate: 2020/1/28 22:42
+ * @Version: 1.0
+ */
+public class BusinessConstants {
+}
