02155c45b8d27db94a04c0dae933a24666d00243
FangChenSheng <frankdevhub@gmail.com>
2020-02-01 21:43:43 +0800

    app spring boot support for project

--- frankdevhub-51job-webserver/src/main/java/frankdevhub/job/automatic/JobWebAutoService.java
+++ frankdevhub-51job-webserver/src/main/java/frankdevhub/job/automatic/JobWebAutoService.java
@@ -1,7 +1,18 @@
 package frankdevhub.job.automatic;
 
-public class JobWebAutoService {
-	public static void main(String[] args) {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import tk.mybatis.spring.annotation.MapperScan;
 
-	}
+@ComponentScan(basePackages = {"frankdevhub.job.automatic"})
+@MapperScan(basePackages = {"frankdevhub.job.automatic.mapper"})
+@EnableTransactionManagement
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class JobWebAutoService {
+    public static void main(String[] args) {
+        SpringApplication.run(JobWebAutoService.class, args);
+    }
 }
---
+++ frankdevhub-51job-webserver/src/main/java/frankdevhub/job/automatic/web/action/JobPlatformDataPatrolService.java
@@ -0,0 +1,15 @@
+package frankdevhub.job.automatic.web.action;
+
+/**
+ * <p>Title:@ClassName JobPlatformDataPatrolService.java</p>
+ * <p>Copyright: Copyright (c) 2020</p>
+ * <p>Company: www.frankdevhub.site</p>
+ * <p>github: https://github.com/frankdevhub</p>
+ *
+ * @Author: frankdevhub@gmail.com
+ * @CreateDate: 2020/2/1 21:37
+ * @Version: 1.0
+ */
+public class JobPlatformDataPatrolService extends Thread {
+
+}
