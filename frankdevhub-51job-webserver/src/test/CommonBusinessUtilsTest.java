4643196087495debd1ff95a4f38b87ec559b9411
FangChenSheng <frankdevhub@gmail.com>
2020-02-02 22:47:33 +0800

    test common business utils

--- frankdevhub-51job-webserver/src/test/CommonBusinessUtilsTest.java
+++ frankdevhub-51job-webserver/src/test/CommonBusinessUtilsTest.java
@@ -1,5 +1,8 @@
-import org.junit.After;
+import frankdevhub.job.automatic.core.data.logging.Logger;
+import frankdevhub.job.automatic.core.data.logging.LoggerFactory;
+import frankdevhub.job.automatic.core.utils.CommonBusinessUtils;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -16,13 +19,76 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class CommonBusinessUtilsTest {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(CommonBusinessUtilsTest.class);
+
+    private void printTestResult(Boolean res, Character c) {
+        System.out.println("Test Character = " + c + " Test Result: " + res.toString() + "");
+    }
+
+    @Test
+    public void getBusinessUtilsDeclaredMethods() {
+        LOGGER.begin().info("run test method {{getBusinessUtilsDeclaredMethods}} start");
+
+
+        LOGGER.begin().info("run test method {{getBusinessUtilsDeclaredMethods}} complete");
+    }
+
+
+    @Test
+    public void testIsSimpleChinese() {
+        LOGGER.begin().info("run test method {{testIsSimpleChinese}} start");
+
+        Character CN_CHAR = '个';
+        Character TW_CHAR = '個';
+        Character EN_CHAR = 'h';
+        Character NUM_CHAR = '3';
+
+        Boolean value = null;
+        try {
+            value = CommonBusinessUtils.isSimpleChinese(CN_CHAR);
+        } catch (Exception e) {
+            e.printStackTrace();
+            value = Boolean.FALSE;
+        } finally {
+            printTestResult(value, CN_CHAR);
+        }
+        try {
+            value = CommonBusinessUtils.isSimpleChinese(TW_CHAR);
+        } catch (Exception e) {
+            e.printStackTrace();
+            value = Boolean.FALSE;
+        } finally {
+            printTestResult(value, TW_CHAR);
+        }
+
+
+        try {
+            value = CommonBusinessUtils.isSimpleChinese(EN_CHAR);
+        } catch (Exception e) {
+            e.printStackTrace();
+            value = Boolean.FALSE;
+        } finally {
+            printTestResult(value, EN_CHAR);
+        }
+
+        try {
+            value = CommonBusinessUtils.isSimpleChinese(NUM_CHAR);
+        } catch (Exception e) {
+            e.printStackTrace();
+            value = Boolean.FALSE;
+        } finally {
+            printTestResult(value, NUM_CHAR);
+        }
+
+
+        LOGGER.begin().info("run test method {{testIsSimpleChinese}} complete");
+    }
+
     @Before
     public void init() {
+        LOGGER.begin().info("invoke {{CommonBusinessUtilsTest:: init()}}");
 
     }
 
-    @After
-    public void after() {
-
-    }
 }
