bdad2d9d2208bdd2985356cc324bdf57417577d8
FangChenSheng <frankdevhub@gmail.com>
2020-02-08 06:12:31 +0800

    parse job search result on the list page and wrapper to job search result entity

--- frankdevhub-51job-webserver/src/main/java/frankdevhub/job/automatic/core/repository/MyBatisRepository.java
+++ frankdevhub-51job-webserver/src/main/java/frankdevhub/job/automatic/core/repository/MyBatisRepository.java
@@ -2,7 +2,7 @@ package frankdevhub.job.automatic.core.repository;
 
 import com.github.pagehelper.PageHelper;
 
-import java.util.Calendar;
+import java.util.TimeZone;
 
 public class MyBatisRepository {
     public void setPage(Integer pageNum, Integer pageSize) {
@@ -10,8 +10,20 @@ public class MyBatisRepository {
             PageHelper.startPage(pageNum, pageSize, true);
     }
 
-    public Long[] getTimeStampRange(Calendar c) {
+    public Long[] getTimeStampRange(Long timeStamp) {
         Long[] range = null;
+        long zeroT = timeStamp / (24 * 3600 * 1000) * (24 * 3600 * 1000) - TimeZone.getDefault().getRawOffset();
+        long endT = zeroT + (24 * 3600 * 1000) - 1;
+
+        range[0] = zeroT;
+        range[1] = endT;
+
+        System.out.println(
+                "timeStamp = "
+                        + timeStamp
+                        + ", zeroT = " + zeroT
+                        + ", endT = " + endT + "");
+
         return range;
     }
 }
