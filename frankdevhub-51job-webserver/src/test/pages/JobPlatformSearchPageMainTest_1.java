cf6a77b9ef87a3fd1e6d817137d7ee1111337df4
FangChenSheng <frankdevhub@gmail.com>
2020-02-13 22:38:30 +0800

    test to use http client to get the search result page information

---
+++ frankdevhub-51job-webserver/src/test/pages/JobPlatformSearchPageMainTest_1.java
@@ -0,0 +1,31 @@
+package pages;
+
+import frankdevhub.job.automatic.core.data.logging.Logger;
+import frankdevhub.job.automatic.core.data.logging.LoggerFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * <p>Title:@ClassName JobPlatformSearchPageMainTest_1.java</p>
+ * <p>Copyright: Copyright (c) 2020</p>
+ * <p>Company: www.frankdevhub.site</p>
+ * <p>github: https://github.com/frankdevhub</p>
+ *
+ * @Author: frankdevhub@gmail.com
+ * @CreateDate: 2020/2/13 22:36
+ * @Version: 1.0
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class JobPlatformSearchPageMainTest_1 {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(JobPlatformSearchPageMainTest_1.class);
+
+    @Test
+    public void testHttpClientGetResultPage() {
+        LOGGER.begin().info("run test method {{testHttpClientGetResultPage}} start");
+
+        LOGGER.begin().info("run test method {{testHttpClientGetResultPage}} complete");
+    }
+}
