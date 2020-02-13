44b325c244cbc746ab60746af197378375b981d4
FangChenSheng <frankdevhub@gmail.com>
2020-02-13 23:44:45 +0800

    test httpclient get search page info

--- frankdevhub-51job-webserver/src/test/pages/JobPlatformSearchPageMainTest_1.java
+++ frankdevhub-51job-webserver/src/test/pages/JobPlatformSearchPageMainTest_1.java
@@ -8,11 +8,19 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.jdom2.Document;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * <p>Title:@ClassName JobPlatformSearchPageMainTest_1.java</p>
@@ -28,18 +36,18 @@ import java.io.IOException;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class JobPlatformSearchPageMainTest_1 {
 
-    private final Logger LOGGER = LoggerFactory.getLogger(JobPlatformSearchPageMainTest_1.class);
-
     private static final String TEST_RESULT_PAGE = "https://search.51job.com/list/020000,000000,0000,00,9,99,java,2,1.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare=";
+    private final Logger LOGGER = LoggerFactory.getLogger(JobPlatformSearchPageMainTest_1.class);
+    private String pageContext = "";
 
-    @Test
-    public void testHttpClientGetResultPage() throws IOException {
-        LOGGER.begin().info("run test method {{testHttpClientGetResultPage}} start");
+    @Before
+    public void init() throws IOException {
+        LOGGER.begin().info("invoke {{JobPlatformSearchPageMainTest_1::init()}}");
 
         Long start = System.currentTimeMillis();
         CloseableHttpClient httpClient = HttpClientBuilder.create().build();
         HttpGet httpGet = new HttpGet(TEST_RESULT_PAGE);
-        httpGet.setHeader("Content-Type", "text/html; charset=UTF-8");
+        httpGet.setHeader("Content-Type", "text/html; charset=GBK");
 
         CloseableHttpResponse response = httpClient.execute(httpGet);
         HttpEntity responseEntity = response.getEntity();
@@ -50,6 +58,24 @@ public class JobPlatformSearchPageMainTest_1 {
 
         System.out.println(responseText);
 
-        LOGGER.begin().info("run test method {{testHttpClientGetResultPage}} complete");
+        this.pageContext = responseText;
     }
+
+    @Test
+    public void testParseByUsingJDomSupport() throws ParserConfigurationException, IOException, SAXException {
+        LOGGER.begin().info("run test method {{testParseByUsingJDomSupport}} start");
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setValidating(false);
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        String context = this.pageContext;
+        InputStream inputStream = new ByteArrayInputStream(context.getBytes());
+
+        //TODO
+        //[Fatal Error] :10:119: The reference to entity "keyword" must end with the ';' delimiter.
+        Document document = (Document) db.parse(inputStream);
+
+        LOGGER.begin().info("run test method {{testParseByUsingJDomSupport}} complete");
+    }
+
 }
