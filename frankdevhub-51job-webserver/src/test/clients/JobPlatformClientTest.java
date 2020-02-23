05423e493d27016370dc24ad42dea6b81260dd0b
FangChenSheng <frankdevhub@gmail.com>
2020-02-23 11:29:13 +0800

    parse job search result page by jsoup and jsoup-xpath support

---
+++ frankdevhub-51job-webserver/src/test/clients/JobPlatformClientTest.java
@@ -0,0 +1,42 @@
+package clients;
+
+import cn.wanghaomiao.xpath.exception.XpathSyntaxErrorException;
+import frankdevhub.job.automatic.core.data.logging.Logger;
+import frankdevhub.job.automatic.core.data.logging.LoggerFactory;
+import frankdevhub.job.automatic.web.clients.JobPlatformClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.IOException;
+
+/**
+ * <p>Title:@ClassName JobPlatformClientTest.java</p>
+ * <p>Copyright: Copyright (c) 2020</p>
+ * <p>Company: www.frankdevhub.site</p>
+ * <p>github: https://github.com/frankdevhub</p>
+ *
+ * @Author: frankdevhub@gmail.com
+ * @CreateDate: 2020/2/23 11:23
+ * @Version: 1.0
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class JobPlatformClientTest {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(JobPlatformClientTest.class);
+
+
+    private final String TEST_RESULT_PAGE = "https://search.51job.com/list/020000,000000,0000,00,9,99,java,2,1.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare=";
+    private final String TEST_SEARCH_KEY = "java";
+
+
+    @Test
+    public void testGetJobSearchResult() throws IOException, XpathSyntaxErrorException {
+        LOGGER.begin().info("run test method {{testGetJobSearchResult}} start");
+        JobPlatformClient client = new JobPlatformClient();
+        client.getJobSearchResult(TEST_RESULT_PAGE, TEST_SEARCH_KEY);
+
+        LOGGER.begin().info("run test method {{testGetJobSearchResult}} complete");
+    }
+}
--- frankdevhub-51job-webserver/src/test/pages/JobPlatformSearchPageMainTest_1.java
+++ frankdevhub-51job-webserver/src/test/pages/JobPlatformSearchPageMainTest_1.java
@@ -41,7 +41,7 @@ import java.util.List;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class JobPlatformSearchPageMainTest_1 {
 
-    private static final String TEST_RESULT_PAGE = "https://search.51job.com/list/020000,000000,0000,00,9,99,java,2,1.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare=";
+    private final String TEST_RESULT_PAGE = "https://search.51job.com/list/020000,000000,0000,00,9,99,java,2,1.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare=";
 
     private final Logger LOGGER = LoggerFactory.getLogger(JobPlatformSearchPageMainTest_1.class);
 
