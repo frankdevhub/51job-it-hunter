70b8cea7c0e068c4c8d4d2feb3adf1e088b1b625
Franklinfang <frankdevhub@gmail.com>
2021-01-29 18:50:52 +0800

    企业招聘人数计划的描述信息解析

--- frankdevhub-51job-webserver/src/main/java/frankdevhub/job/automatic/core/parser/HeadCountTextUtils.java
+++ frankdevhub-51job-webserver/src/main/java/frankdevhub/job/automatic/core/parser/HeadCountTextUtils.java
@@ -1,5 +1,6 @@
 package frankdevhub.job.automatic.core.parser;
 
+import frankdevhub.job.automatic.entities.JobSearchResult;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -16,5 +17,30 @@ import lombok.extern.slf4j.Slf4j;
 public class HeadCountTextUtils {
 
     //上海 | 无需经验 | 本科 | 招若干人
+    //匹配招聘人数的正则表达式
+    private static final String REGEX_EXAMPLE = ".*(?<prefix>[招聘|招纳|招|需要|急需|需]+)" +
+            "(?<numeric>[\u4e00-\u9fa5\u767e\u5343\u96f6]+|[0-9]+|[若干])(?<surfix>人)$";
 
+    /**
+     * 匹配招聘人数
+     *
+     * @param text 描述信息
+     * @return 计划的招聘人数
+     */
+    public static String parseText(String text) {
+        String match = null;
+        return match;
+    }
+
+    /**
+     * 匹配招聘人数
+     *
+     * @param text   描述信息
+     * @param result 职位搜索返回的结果集
+     * @return 计划的招聘人数
+     */
+    public static void parseText(String text, JobSearchResult result) {
+        String match = null;
+
+    }
 }
