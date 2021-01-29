28eb5ec9742a93e5c0f0a9cbd679177d1ebaf577
Franklinfang <frankdevhub@gmail.com>
2021-01-29 19:49:34 +0800

    企业招聘人数计划的描述信息解析

--- frankdevhub-51job-webserver/src/main/java/frankdevhub/job/automatic/core/parser/HeadCountTextUtils.java
+++ frankdevhub-51job-webserver/src/main/java/frankdevhub/job/automatic/core/parser/HeadCountTextUtils.java
@@ -1,7 +1,12 @@
 package frankdevhub.job.automatic.core.parser;
 
+import frankdevhub.job.automatic.core.utils.NumericStringUtils;
 import frankdevhub.job.automatic.entities.JobSearchResult;
 import lombok.extern.slf4j.Slf4j;
+import tk.mybatis.mapper.util.Assert;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @Title: HeadCountTextUtils
@@ -18,8 +23,12 @@ public class HeadCountTextUtils {
 
     //上海 | 无需经验 | 本科 | 招若干人
     //匹配招聘人数的正则表达式
-    private static final String REGEX_EXAMPLE = ".*(?<prefix>[招聘|招纳|招|需要|急需|需]+)" +
+    private static final String REGEX_EXPRESSION = ".*(?<prefix>[招聘|招纳|招|需要|急需|需]+)" +
             "(?<numeric>[\u4e00-\u9fa5\u767e\u5343\u96f6]+|[0-9]+|[若干])(?<surfix>人)$";
+    //特殊情况 若干
+    private static final String EXAMPLE_1 = "若干";
+    //正则表达式中文字符匹配
+    private static final String REGEX_CH = "[\u4e00-\u9fa5]";
 
     /**
      * 匹配招聘人数
@@ -28,7 +37,16 @@ public class HeadCountTextUtils {
      * @return 计划的招聘人数
      */
     public static String parseText(String text) {
+        Assert.notNull(text, "cannot find text");
+        log.info("text = {}", text);
         String match = null;
+
+        //过滤去除多余的空格换行符
+        text = text.replaceAll("\\s|\\t|\\n", "");
+        Pattern p = Pattern.compile(REGEX_EXPRESSION);
+        Matcher m = p.matcher(text);
+        if (m.find())
+            match = m.group("numeric");
         return match;
     }
 
@@ -40,7 +58,28 @@ public class HeadCountTextUtils {
      * @return 计划的招聘人数
      */
     public static void parseText(String text, JobSearchResult result) {
-        String match = null;
+        Assert.notNull(text, "cannot find text");
+        log.info("text = {}", text);
 
+        //匹配关键字
+        String match = parseText(text);
+        if (null == match)
+            return;
+
+        //如果匹配到关键字,进行判断是中文还是数字
+        Pattern p = Pattern.compile(REGEX_CH);
+        //过滤去除多余的空格换行符
+        match = match.replaceAll("\\s|\\t|\\n", "");
+        Matcher m = p.matcher(match);
+        int num = 0;
+        //中文字符
+        if (m.find()) {
+            //非大写中文数字转为阿拉伯数字
+            num = NumericStringUtils.numberCN2Arab(match);
+        } else {
+            //数值类型字符
+            num = Integer.parseInt(match);
+        }
+        result.setHeadCount(num);
     }
 }
