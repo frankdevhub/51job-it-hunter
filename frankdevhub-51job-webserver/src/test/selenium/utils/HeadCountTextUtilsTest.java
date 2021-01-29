13ecb5666e913712de804c4bf7659cfdec32606e
Franklinfang <frankdevhub@gmail.com>
2021-01-29 18:42:34 +0800

    企业招聘人数计划的描述信息解析

--- frankdevhub-51job-webserver/src/test/selenium/utils/HeadCountTextUtilsTest.java
+++ frankdevhub-51job-webserver/src/test/selenium/utils/HeadCountTextUtilsTest.java
@@ -2,15 +2,13 @@ package selenium.utils;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * @Title: HeadCountTextUtilsTest
- * @Description: //TODO
+ * @Description: 测试解析招聘人数的正则表达式
  * @date: 2021/1/28 22:54
  * @author: frankdevhub@gmail.com
  * @blog: blog.frankdevhub.site
@@ -18,17 +16,40 @@ import java.util.regex.Pattern;
  */
 
 @Slf4j
-@RunWith(SpringJUnit4ClassRunner.class)
 @SuppressWarnings("all")
 public class HeadCountTextUtilsTest {
 
     //上海 | 无需经验 | 本科 | 招若干人
-    private final String REGEX_EXAMPLE = "^(招聘|招纳|招|需要|需){0,1}" +
-            "(?<numeric>([\\u4e00-\\u9fa5\\u767e\\u5343\\u96f6]|[0-9]|若干)*)(人{0,1})$"; //匹配招聘人数的正则表达式
+    //匹配招聘人数的正则表达式
+    private final String REGEX_EXAMPLE = ".*(?<prefix>[招聘|招纳|招|需要|急需|需]+)" +
+            "(?<numeric>[\u4e00-\u9fa5\u767e\u5343\u96f6]+|[0-9]+|[若干])(?<surfix>人)$";
 
-    private final String[] examples = new String[]{"招 23 人", "招 1 人", "招一人",
-            "上海 | 无需经验 | 本科 | 招若干人",
-            "上海 | 无需经验 招若干人"};
+    private final String[] examples = new String[]{"招 23 人",  //十位数及以上的型数值
+            "招若干人", //若干人
+            "招 1 人",  //个位数值
+            "招一人",   //文字类型数值
+            "上海 | 无需经验 | 本科 | 招若干人", //若干人
+            "上海 | 无需经验 招若干人"};  // 若干人
+
+    /**
+     * 测试匹配中文前缀
+     */
+    @Test
+    public void testMatchPrefix() {
+        String regex = ".*(?<prefix>[招聘|招纳|招|需要|急需|需]+)";
+        for (String example : examples) {
+            //替换多余的空格换行字符
+            String parse = example.replaceAll("(\\s|\\t|\\n)", "");
+            Pattern p = Pattern.compile(regex);
+            Matcher m = p.matcher(parse);
+            log.info("parse = {}", parse);
+            if (m.find()) {
+                log.info("match = {}", m.group("prefix"));
+            } else {
+                log.info("not match");
+            }
+        }
+    }
 
     /**
      * 测试解析招聘人数的正则表达式
@@ -36,17 +57,18 @@ public class HeadCountTextUtilsTest {
     @Test
     public void testRegexExpression() {
         for (String example : examples) {
-            String parse = example.trim().replaceAll("(\\s|\\t|\\n)", ""); //去除多余空格
+            //替换多余的空格换行字符
+            String parse = example.replaceAll("(\\s|\\t|\\n)", "");
             Pattern p = Pattern.compile(REGEX_EXAMPLE);
             Matcher m = p.matcher(parse);
             log.info("parse = {}", parse);
-
             if (m.find()) {
                 log.info("match = {}", m.group("numeric"));
             } else {
-                log.info("cannot match");
+                log.info("not match");
             }
         }
     }
+
 
 }
