f9a469d96b3c70e548f8836173befe5100c293b8
Franklinfang <frankdevhub@gmail.com>
2021-02-03 12:44:47 +0800

    测试Htmlunit获取企业信息介绍页面html源码字符串

--- frankdevhub-51job-webserver/src/test/data/clients/JobCompanyProfileTest.java
+++ frankdevhub-51job-webserver/src/test/data/clients/JobCompanyProfileTest.java
@@ -41,25 +41,20 @@ public class JobCompanyProfileTest {
      */
     @Test
     public void testGetPageHtmlTextByHtmlUnit() throws IOException {
-        //新建一个模拟谷歌Chrome浏览器的浏览器客户端对象
-        WebClient webClient = new WebClient(BrowserVersion.CHROME);
-        //当JS执行出错的时候是否抛出异常, 这里选择不需要
-        webClient.getOptions().setThrowExceptionOnScriptError(false);
-        //当HTTP的状态非200时是否抛出异常, 这里选择不需要
-        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+
+        WebClient webClient = new WebClient(BrowserVersion.CHROME); //新建一个模拟谷歌Chrome浏览器的浏览器客户端对象
+        webClient.getOptions().setThrowExceptionOnScriptError(false);//当JS执行出错的时候是否抛出异常, 这里选择不需要
+        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);//当HTTP的状态非200时是否抛出异常, 这里选择不需要
         webClient.getOptions().setActiveXNative(false);
-        //是否启用CSS, 因为不需要展现页面, 所以不需要启用
-        webClient.getOptions().setCssEnabled(false);
-        //很重要，启用JS。有些网站要开启！
-        webClient.getOptions().setJavaScriptEnabled(true);
-        //很重要，设置支持AJAX
-        webClient.setAjaxController(new NicelyResynchronizingAjaxController());
-        webClient.getOptions().setRedirectEnabled(false);
-        webClient.getCache().setMaxSize(100);
+        webClient.getOptions().setCssEnabled(false);//是否启用CSS, 因为不需要展现页面, 所以不需要启用
+        webClient.getOptions().setJavaScriptEnabled(true); //很重要，启用JS
+        webClient.setAjaxController(new NicelyResynchronizingAjaxController());//很重要，设置支持AJAX
         //获取页面对象
         HtmlPage page = webClient.getPage(TEST_PAGE);
+        webClient.close();
+        webClient.waitForBackgroundJavaScript(30000);//异步JS执行需要耗时,所以这里线程要阻塞30秒,等待异步JS执行结束
         //获取页面对象的字符串源码
-        String context = page.getTextContent();
+        String context = page.asXml();//直接将加载完成的页面转换成xml格式的字符串
         System.out.println(context);
 
     }
