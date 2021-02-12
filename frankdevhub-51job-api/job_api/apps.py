47f6338a5a378f6618ac2852b032d684ee46364c
Franklinfang <frankdevhub@gmail.com>
2021-02-12 20:29:48 +0800

    #django-admin appstart

---
+++ frankdevhub-51job-api/job_api/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+
+
+# !/usr/bin/env python
+# encoding: utf-8
+# @author: frankdevhub
+# @contact: frankdevhub@gmail.com
+# @blog: http://blog.frankdevhub.site
+# @time: 2021/2/12 16:27
+
+class JobApiConfig(AppConfig):
+    name = 'job_api'
