32ed9b28ef7e377e57886a2d24c5aff32cb6304c
Franklinfang <frankdevhub@gmail.com>
2021-02-13 13:57:01 +0800

    #unittest:解析匹配的正则表达式测试

--- frankdevhub-51job-api/frankdevhub_51job_api/unittest/__init__.py
+++ frankdevhub-51job-api/frankdevhub_51job_api/unittest/__init__.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 # encoding: utf-8
 # @author: frankdevhub
 # @contact: frankdevhub@gmail.com
 # @blog: http://blog.frankdevhub.site
-# @file: __init__.py.py
-# @time: 2021/2/12 22:48
-# @desc:
+# @time: 2021/2/12 16:27
