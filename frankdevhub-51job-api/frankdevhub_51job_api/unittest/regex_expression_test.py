7fd7f8f749e31e28fd086b8b133560c3ad1bee8c
Franklinfang <frankdevhub@gmail.com>
2021-02-13 18:18:19 +0800

    #Fixed: re.compile(HEAD_COUNT_REGEX, re.M | re.I) match匹配无结果

--- frankdevhub-51job-api/frankdevhub_51job_api/unittest/regex_expression_test.py
+++ frankdevhub-51job-api/frankdevhub_51job_api/unittest/regex_expression_test.py
@@ -15,23 +15,27 @@ HEAD_COUNT_REGEX = """
 .*(?P<prefix>[招聘|招纳|招|需要|急需|需]+)(?P<numeric>[\u4e00-\u9fa5\u767e\u5343\u96f6]+|[0-9]+|[若干])(?P<surfix>人)$
 """
 
-TEST_HEAD_COUNT = "招 23 人"
+TEST_HEAD_COUNT = " 招纳23人"
 
 
-class TestRegexExpression(unittest.TestCase):
+class RegexExpressionTest(unittest.TestCase):
 
-    def match_head_count(self):
+    def test_match_head_count(self):
         print('invoke match_head_count')
-        matched = re.match(HEAD_COUNT_REGEX, TEST_HEAD_COUNT, re.M | re.I)
+        print(f'example string = {TEST_HEAD_COUNT}')
+
+        filter_string = re.sub('\\t|\\s|\\n', '', TEST_HEAD_COUNT, re.M | re.I)  # 去除换行符空格符
+        print(f'filtered string = {filter_string}')
+
+        pattern = re.compile(HEAD_COUNT_REGEX, re.M | re.I)
+        matched = pattern.match(filter_string)
         if matched:
-            print
-            "prefix :", matched.group('prefix')
+            print(f'prefix = {matched.group("prefix")}')  # prefix
+            print(f'numeric = {matched.group("numeric")}')  # numeric
+            print(f'surfix = {matched.group("surfix")}')  # surfix
         else:
-            print('no matched')
+            print('mot matched')
 
 
 if __name__ == "__main__":
-    testunit = unittest.TestSuite()
-    testunit.addTest(TestRegexExpression("match_head_count"))
-    runner = unittest.TextTestRunner()
-    runner.run(testunit)
+    unittest.main()
