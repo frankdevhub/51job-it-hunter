1e96aa205c811636f897de73f5069ddc28982001
Franklinfang <frankdevhub@gmail.com>
2021-02-13 13:09:56 +0800

    #unittest:解析匹配的正则表达式测试

--- frankdevhub-51job-api/frankdevhub_51job_api/unittest/regex_expression_test.py
+++ frankdevhub-51job-api/frankdevhub_51job_api/unittest/regex_expression_test.py
@@ -7,6 +7,7 @@
 # @time: 2021/2/13 12:24
 # @desc: 解析匹配的正则表达式测试
 
+import re
 import unittest
 
 """企业岗位招聘人数的正则表达式 eg:(招1人)"""
@@ -14,13 +15,23 @@ HEAD_COUNT_REGEX = """
 .*(?<prefix>[招聘|招纳|招|需要|急需|需]+)(?<numeric>[\u4e00-\u9fa5\u767e\u5343\u96f6]+|[0-9]+|[若干])(?<surfix>人)$
 """
 
+TEST_HEAD_COUNT = "招 23 人"
+
 
 class TestRegexExpression(unittest.TestCase):
-    pass
+
+    def match_head_count(self):
+        print('invoke match_head_count')
+        matched = re.match(HEAD_COUNT_REGEX, TEST_HEAD_COUNT, re.M | re.I)
+        if matched:
+            print
+            "prefix :", matched.group('prefix')
+        else:
+            print('no matched')
 
 
 if __name__ == "__main__":
     testunit = unittest.TestSuite()
-    testunit.addTest(TestRegexExpression('parse_head_count'))
+    testunit.addTest(TestRegexExpression('match_head_count'))
     runner = unittest.TextTestRunner()
     runner.run(testunit)
