0a8a1f721d4e77e51500ed4fb0318c577c1e9661
Franklinfang <frankdevhub@gmail.com>
2021-02-13 14:23:13 +0800

    #python django

--- frankdevhub-51job-api/frankdevhub_51job_api/urls.py
+++ frankdevhub-51job-api/frankdevhub_51job_api/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
-from ..job_api.views import echo_index
+from job_api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('echo/', echo_index)
+    path('echo/', views.echo_index)
 ]
