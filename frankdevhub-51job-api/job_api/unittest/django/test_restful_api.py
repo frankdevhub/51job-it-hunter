8aad3e246f4f42af34766713478a31279fe3416d
Franklinfang <frankdevhub@gmail.com>
2021-02-14 23:10:25 +0800

    #api注册路由并进行绑定

--- frankdevhub-51job-api/job_api/unittest/django/test_restful_api.py
+++ frankdevhub-51job-api/job_api/unittest/django/test_restful_api.py
@@ -1,8 +1,6 @@
-import unittest
-
 from requests import Response
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
 
 from job_api.models import PlatformDataBriefSource
 from job_api.serializers import PlatDataJsonSerializer
@@ -17,16 +15,25 @@ from job_api.serializers import PlatDataJsonSerializer
 # @time: 2021/2/13 23:09
 # @desc: restful API 功能测试
 
-@api_view(['GET'])
-def get_platform_data_count(request):
-    print('invoke method -> get_platform_data_count()')
-    total_rows = PlatformDataBriefSource.objects.count()
-    total_serializer = PlatDataJsonSerializer(total_rows, many=False)
 
-    print(f'total_rows = {total_rows}')
+class PlatDataJsonViewSet(viewsets.ModelViewSet):
+    queryset = PlatformDataBriefSource.objects.all()
+    serializer_class = PlatDataJsonSerializer
 
-    return Response(data=total_serializer.data, status=status.HTTP_200_OK)
+    # action是drf提供的路由和视图方法绑定关系的装饰器
+    # from rest_framework.decorators import action
+    # 参数1: methods 列表，设置视图方法允许哪些http请求访问进来
+    # 参数2: detail  当前是否方法是否属于详情页视图，
+    #        False，系统不会自动增加pk在生成的路由地址中
+    #        True  则系统会自动增加pk在生成的路由地址
 
-
-if __name__ == '__main__':
-    unittest.main()
+    @action(methods=['get'], detail=False)  # detail=False 是否为详情页数据
+    def get_platform_data_count(self, request):  # 其接口 http://127.0.0.1:port/api/plat_data/get_platform_data_count/
+        print('invoke method -> get_platform_data_count()')
+        if request.method == 'GET':
+            total_rows = PlatformDataBriefSource.objects.count()
+            total_serializer = PlatDataJsonSerializer(total_rows, many=False)
+            print(f'total_rows = {total_rows}')
+            return Response(data=total_serializer.data, status=status.HTTP_200_OK)
+        else:
+            pass
