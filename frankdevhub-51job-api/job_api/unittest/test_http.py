697af20f712f7b520e093e7936c9995c85e82370
Franklinfang <frankdevhub@gmail.com>
2021-02-14 11:18:03 +0800

    #unittest: Http 请求测试

---
+++ frankdevhub-51job-api/job_api/unittest/test_http.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+# !/usr/bin/env python
+# encoding: utf-8
+# @author: frankdevhub
+# @contact: frankdevhub@gmail.com
+# @blog: http://blog.frankdevhub.site
+# @file: test_http.py
+# @time: 2021/2/14 11:11
+# @desc:  Http 请求测试
+
+class TestHttpRequest(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
