f70ffba1397da6a20f3b1fcc72913b8b799e81b7
Franklinfang <frankdevhub@gmail.com>
2021-02-14 23:31:51 +0800

    #api注册路由并进行绑定

---
+++ frankdevhub-51job-api/job_api/unittest/django/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# @author: frankdevhub
+# @contact: frankdevhub@gmail.com
+# @blog: http://blog.frankdevhub.site
+# @file: __init__.py.py
+# @time: 2021/2/13 21:39
+# @desc:
