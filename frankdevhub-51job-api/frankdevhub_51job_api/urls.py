248756ff3a029b07597697b835d996441bf022a0
Franklinfang <frankdevhub@gmail.com>
2021-02-14 23:11:37 +0800

    #api注册路由并进行绑定

--- frankdevhub-51job-api/frankdevhub_51job_api/urls.py
+++ frankdevhub-51job-api/frankdevhub_51job_api/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from job_api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('echo/', views.echo_index)
+    path('echo/', views.echo_index),
+    path('api/', include('job_api.urls'))  # 绑定子应用
 ]
