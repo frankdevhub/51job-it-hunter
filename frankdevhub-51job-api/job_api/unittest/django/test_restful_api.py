20fe47a4f7fd02e7c88514597ab874cd12033d85
Franklinfang <frankdevhub@gmail.com>
2021-02-14 23:21:42 +0800

    #api注册路由并进行绑定

--- frankdevhub-51job-api/job_api/unittest/django/test_restful_api.py
+++ frankdevhub-51job-api/job_api/unittest/django/test_restful_api.py
@@ -28,7 +28,7 @@ class PlatDataJsonViewSet(viewsets.ModelViewSet):
     #        True  则系统会自动增加pk在生成的路由地址
 
     @action(methods=['get'], detail=False)  # detail=False 是否为详情页数据
-    def get_platform_data_count(self, request):  # 其接口 http://127.0.0.1:port/api/plat_data/get_platform_data_count/
+    def get_platform_data_count(self, request):  # 其接口 http://127.0.0.1:port/job_api/plat_data/get_platform_data_count/
         print('invoke method -> get_platform_data_count()')
         if request.method == 'GET':
             total_rows = PlatformDataBriefSource.objects.count()
