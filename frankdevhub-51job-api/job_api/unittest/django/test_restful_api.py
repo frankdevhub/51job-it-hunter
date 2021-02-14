71f3d61cc64bf6f25a8a47d7f512017c282af8e6
Franklinfang <frankdevhub@gmail.com>
2021-02-14 22:31:04 +0800

    #unittets: restful API 功能测试

--- frankdevhub-51job-api/job_api/unittest/django/test_restful_api.py
+++ frankdevhub-51job-api/job_api/unittest/django/test_restful_api.py
@@ -1,6 +1,11 @@
 import unittest
 
-from job_api.models import *
+from requests import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+from job_api.models import PlatformDataBriefSource
+from job_api.serializers import PlatDataJsonSerializer
 
 
 # !/usr/bin/env python
@@ -12,13 +17,15 @@ from job_api.models import *
 # @time: 2021/2/13 23:09
 # @desc: restful API 功能测试
 
-
+@api_view(['GET'])
 def get_platform_data_count(request):
     print('invoke method -> get_platform_data_count()')
     total_rows = PlatformDataBriefSource.objects.count()
+    total_serializer = PlatDataJsonSerializer(total_rows, many=False)
+
     print(f'total_rows = {total_rows}')
 
-    return None
+    return Response(data=total_serializer.data, status=status.HTTP_200_OK)
 
 
 if __name__ == '__main__':
