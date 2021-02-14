b05f1aef817f64ee7208c66ca5c1e6de741918ee
Franklinfang <frankdevhub@gmail.com>
2021-02-14 17:31:27 +0800

    #unittets: view模块中测试接口功能:Django-ORM的实现

--- frankdevhub-51job-api/job_api/views.py
+++ frankdevhub-51job-api/job_api/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 
+from job_api.models import PlatformDataBriefSource
+
 
 # !/usr/bin/env python
 # encoding: utf-8
@@ -8,12 +10,19 @@ from django.http import HttpResponse
 # @blog: http://blog.frankdevhub.site
 # @time: 2021/2/12 16:27
 
-# Create your views here.
-
 
 def echo_index(request):
     content = '<h3>Welcome, frankdevhub@gmail.com</h3>'
     return HttpResponse(content)
+
+
+def get_platform_data_count(request):
+    print('invoke method -> get_platform_data_count()')
+    total_rows = PlatformDataBriefSource.objects.count()
+    print(f'total_rows = {total_rows}')
+
+    # TODO: None 需要替换空的视图会产生运行时异常
+    return None
 
 
 if __name__ == "__main__":
