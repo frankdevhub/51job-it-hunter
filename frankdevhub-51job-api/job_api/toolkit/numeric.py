0f8828f60028c9177133be3464479f1a66c78b57
Franklinfang <frankdevhub@gmail.com>
2021-02-18 15:48:06 +0800

    # toolkit: 上海市各辖区单位以及区编号的枚举类

---
+++ frankdevhub-51job-api/job_api/toolkit/numeric.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@File ：numeric.py
+@Author ：frankdevhub@gmail.com
+@Blog : http://blog.frankdevhub.site
+@Date ：2021/2/18 14:51
+"""
---
+++ frankdevhub-51job-api/job_api/toolkit/salary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@File ：salary.py
+@Author ：frankdevhub@gmail.com
+@Blog : http://blog.frankdevhub.site
+@Date ：2021/2/18 14:47
+"""
+
+
+class SalaryText:
+    pass
