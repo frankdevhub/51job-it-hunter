19c50de9329d4a638573628f1a126d12e0a709e9
Franklinfang <frankdevhub@gmail.com>
2021-02-19 19:31:17 +0800

    # toolkit: 数值类字符枚举类测试

---
+++ frankdevhub-51job-api/job_api/unittest/toolkit/test_numeric.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@File ：test_numeric.py
+@Author ：frankdevhub@gmail.com
+@Blog : http://blog.frankdevhub.site
+@Date ：2021/2/19 19:06
+"""
+
+import logging as log
+import unittest
+
+log.basicConfig(level=log.INFO)
+
+
+class TestNumericUnit(unittest.TestCase):
+    def test_unit(self):
+        self.fail()
+
+    def test_unit(self):
+        self.fail()
+
+    def test_is_capital(self):
+        self.fail()
+
+    def test_is_capital(self):
+        self.fail()
+
+    def test_is_en(self):
+        self.fail()
+
+    def test_is_en(self):
+        self.fail()
+
+    def test_is_tw(self):
+        self.fail()
+
+    def test_is_tw(self):
+        self.fail()
+
+    def test_is_cn(self):
+        self.fail()
+
+    def test_is_cn(self):
+        self.fail()
+
+
+if __name__ == '__main__':
+    pass
