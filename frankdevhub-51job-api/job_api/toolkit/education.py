591ed5b1de9fb2c5d6a8b317b353ea61594ef5d7
Franklinfang <frankdevhub@gmail.com>
2021-02-19 19:30:53 +0800

    # toolkit: 教育学历文化水平枚举类

---
+++ frankdevhub-51job-api/job_api/toolkit/education.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@File ：education.py
+@Author ：frankdevhub@gmail.com
+@Blog : http://blog.frankdevhub.site
+@Date ：2021/2/19 19:22
+"""
+
+from enum import Enum, unique
+
+
+@unique
+class EducationDegree(Enum):
+    """教育学历文化水平"""
+    pass
