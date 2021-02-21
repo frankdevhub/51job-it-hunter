dbaf596d4322d13ecbd43c8dd1e2fced0392189d
Franklinfang <frankdevhub@gmail.com>
2021-02-21 17:31:42 +0800

    # pageSettings自定义分页参数配置

--- frankdevhub-51job-api/job_api/toolkit/numeric.py
+++ frankdevhub-51job-api/job_api/toolkit/numeric.py
@@ -6,8 +6,13 @@
 @Blog : http://blog.frankdevhub.site
 @Date ：2021/2/18 14:51
 """
-
+import inspect
+import logging as log
 from enum import Enum, unique
+
+from job_api.toolkit.character import CharacterHelper
+
+log.basicConfig(level=log.INFO)
 
 
 @unique
@@ -15,44 +20,29 @@ class NumericUnit(Enum):
     """ 通用计量单位枚举类"""
 
     def __new__(cls, unit: chr):
-        cls._is_capital, cls._is_en, cls._is_tw, cls._is_cn, cls._unit = unit
+        instance = object.__new__(cls)
+        instance.unit = unit
+        instance_members = [(obj, type(obj)) for (type_name, obj) in inspect.getmembers(CharacterHelper) if
+                            inspect.isfunction(obj)]
 
-    @property
-    def unit(self):
-        return self._unit
+        for inst in instance_members:
+            func = inst[0]
+            log.info(f'function_name: {func.__name__} ,arg_count: {func.__code__.co_argcount}')
+            fun_name = func.__name__
+            if fun_name.lstrip().startswith('is_'):
+                bool_res = func(unit)
+                assert isinstance(bool_res, bool)
+                setattr(instance, func.__name__, bool_res)
+                log.info(f'function_name: {func.__name__} ,bool_res: {bool_res}')
+        return instance
 
-    @unit.getter
-    def unit(self, value):
-        pass
-
-    @property
-    def is_capital(self):
-        return self.is_capital
-
-    @unit.getter
-    def is_capital(self, value):
-        pass
-
-    @property
-    def is_en(self):
-        return self.is_en
-
-    @unit.getter
-    def is_en(self, value):
-        pass
-
-    @property
-    def is_tw(self):
-        return self.is_en
-
-    @unit.getter
-    def is_tw(self, value):
-        pass
-
-    @property
-    def is_cn(self):
-        return self.is_en
-
-    @unit.getter
-    def is_cn(self, value):
-        pass
+    Digitis_CN = '个'  # 个位数 简体中文
+    Digitis_TW = '個'  # 个位数 繁体中文
+    Ten_Digitis_CN_TW = '十'  # 十位数 简体中文
+    Hundred_CN = '百'  # 百位数 简体中文
+    Hundred_TW = '百'  # 百位数 繁体中文
+    Thousand_CN_TW = '千'  # 千位数 繁体中文
+    Thousand_EN = 'K'  # 千位数 英文字符
+    Ten_Thousand_CN = '万'  # 万位数 中文简体
+    Ten_Thousand_TW = '萬'  # 万位数 繁体中文
+    Ten_Thousand_EN = 'W'  # 万位数 英文字符
