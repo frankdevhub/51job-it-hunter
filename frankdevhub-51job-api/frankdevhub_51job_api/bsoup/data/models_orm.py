51ee292c02a211865342a76b79de2081f7bc9fd6
frankdevhub <frankdevhub@gmail.com>
2021-03-14 22:53:56 +0800

    # unittest: sqlalchemy orm 实体类持久化单元测试

--- frankdevhub-51job-api/frankdevhub_51job_api/bsoup/data/models_orm.py
+++ frankdevhub-51job-api/frankdevhub_51job_api/bsoup/data/models_orm.py
@@ -6,33 +6,19 @@
 @Blog : http://blog.frankdevhub.site
 @Date ：2021/3/7 22:23
 """
-# 为了使用MySQL支持的类型，如unsigned bigint
 import sqlalchemy as db
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# 创建对象的基类:
 Base = declarative_base()
 
 
-# 初始化数据库连接:
-# 连接字符串模式：数据库类型+连接库+用户名+密码+主机，字符编码，是否打印建表细节
-# 其中，连接库是当前用于操作数据库的库，对于python2.7，一般使用MysqlDb，对于Python3，一般使用pymysql
-# 连接的例子如：create_engine("mysql+pymysql://cai:123@localhost/test?charset=utf8", echo=True)
-# engine = db.create_engine('mysql+pymysql://root:frank#0806db@ecs@39.98.246.50:3306/51job_data_center')
-
-
-# 删除现有的表，谨慎决定是否需要这样操作
 # Base.metadata.drop_all(engine)
 
-# 创建表
 # Base.metadata.create_all(engine)
 
 def get_session():
-    db_session = sessionmaker(bind=engine)
-    return db_session
-
-
-def get_engine():
     engine = db.create_engine('mysql+pymysql://root:frank#0806db@ecs@39.98.246.50:3306/51job_data_center')
-    return engine
+    assert engine is not None, f'db engine cannot be empty'
+    session = sessionmaker(bind=engine)
+    return session
--- frankdevhub-51job-api/frankdevhub_51job_api/bsoup/data/test_models_orm.py
+++ frankdevhub-51job-api/frankdevhub_51job_api/bsoup/data/test_models_orm.py
@@ -8,19 +8,33 @@
 """
 
 import logging as log
+import unittest
+
+from frankdevhub_51job_api.bsoup.data.models import *
+from frankdevhub_51job_api.bsoup.data.models_orm import *
 
 log.basicConfig(level=log.DEBUG)
 
 
-class TestModelORM(TestCase.unittest):
-    def test_get_session(self):
-        self.fail()
+class TestModelORM(unittest.TestCase):
 
-    def test_get_engine(self):
-        self.fail()
+    def test_get_session(self):
+        log.debug('invoke method -> test_get_session()')
+        session = get_session()
+        log.debug(f'session = {session}')
+        assert session is not None, f'session cannot be empty'
+
+    def test_query_count(self):
+        log.debug('invoke method -> test_query_count()')
+        session = get_session()
+        query = session.query(PlatformDataBriefSource.id)
+        rows = query.count()
+        log.debug(f'query params = PlatformDataBriefSource, result rows = {rows}')
 
 
 if __name__ == '__main__':
     test_suite = unittest.TestSuite()
     test_suite.addTest(TestModelORM('test_get_session'))  # test_get_session
-    test_suite.addTest(TestModelORM('test_get_engine'))  # test_get_engine
+    test_suite.addTest(TestModelORM('test_query_count'))  # test_query_count
+    runner = unittest.TextTestRunner()
+    runner.run(test_suite)
